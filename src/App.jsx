ffdfa106abf5bc96c59a4dbf11448072d1769b50
Raherinotoavina <raherinotoavinasa@gmail.com>
2024-06-08 09:57:29 +0300

    @Raherinotoavina : configure socketio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RoomEtu from "./Etudiants/pages/RoomEtu";
 
+import { SocketIOProvider } from "./context/SocketIOContext";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<RoomEtu />} />
+                <Route
+                    path="/"
+                    element={
+                        <SocketIOProvider>
+                            <RoomEtu />
+                        </SocketIOProvider>
+                    }
+                />
             </Routes>
         </BrowserRouter>
     );
--- src/context/SocketIOContext.jsx
+++ src/context/SocketIOContext.jsx
@@ -1,9 +1,26 @@
 import { createContext, useContext } from "react";
+import { useEffect, useRef, useState } from "react";
+import { io } from "socket.io-client";
 
 const socketIOContext = createContext();
 
 function SocketIOProvider({ children }) {
-    return <socketIOContext.Provider>{children}</socketIOContext.Provider>;
+    const [isSocketInitialized, setIsSocketInitialized] = useState(false);
+    const socketIO = useRef(null);
+
+    useEffect(function () {
+        socketIO.current = io(import.meta.env.VITE_BACKEND_SERVER);
+        socketIO.current.on("connect", function () {
+            console.log("a user connected.");
+        });
+        setIsSocketInitialized(true);
+    }, []);
+
+    return (
+        <socketIOContext.Provider value={{ socketIO, isSocketInitialized }}>
+            {children}
+        </socketIOContext.Provider>
+    );
 }
 
 function useSocketIO() {
